762a75df733c9889ae72c484996129c82c44f19a
laffey <84133272+misaka23300@users.noreply.github.com>
2024-07-27 21:15:49 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: 'zh-CN',
 
   title: 'VuePress',
   description: 'My first VuePress Site',
