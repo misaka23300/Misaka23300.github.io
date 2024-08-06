141c13ce8642c51eed7a82fb26b4d5be6e49b6a6
laffey <84133272+misaka23300@users.noreply.github.com>
2024-08-06 21:22:11 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,17 @@
 module.exports = {
   themeConfig: {
-    lang: 'zh-CN',
-    // 或者
-    
+    lang: {
+      home: '首页',
+      posts: '文章',
+      category: '分类',
+      categories: '分类',
+      allCategories: '全部',
+      tag: '标签',
+      tags: '标签',
+      createdAt: '发布时间',
+      updatedAt: '最后修改',
+      prevPost: '上一篇',
+      nextPost: '下一篇',
+    },
   },
 }
