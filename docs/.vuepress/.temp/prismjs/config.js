3f5d2da05f11df424a347eff83f7ebd1e1fdc6ed
misaka23300 <84133272+misaka23300@users.noreply.github.com>
2024-08-06 13:08:44 +0000

    Deploying to main from @ misaka23300/Misaka23300.github.io@95944b2dafbede86855dd9de25749ab9557f7300 🚀

--- docs/.vuepress/.temp/prismjs/config.js
+++
@@ -1,3 +0,0 @@
-import "C:/code_7tanda/vuepress/vuepress-text/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
-import "C:/code_7tanda/vuepress/vuepress-text/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
-import "C:/code_7tanda/vuepress/vuepress-text/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
--- old/post.php
+++
@@ -1,2 +0,0 @@
-欢迎 <?php echo $_REQUEST["fname"]; ?>!<br>
-你的年龄是 <?php echo $_REQUEST["age"]; ?>  岁
